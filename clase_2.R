0ded6c6ec0c6093fc7115fe0c48e18d412e0cdc7
David Israel Macas Serrano <David@MacBook-Air-de-David-3.local>
2020-08-23 13:05:37 -0500

    agregue rmd para explorar plantas extintas

---
+++ clase_2.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(kableExtra)
+library(knitr)
+
+plants <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv")
+actions <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv")
+threats <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv")
